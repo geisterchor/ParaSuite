2050c95b01a5649c1d91e2bd1883c2f35a54f5c8
Malte Janduda <mail@janduda.net>
2014-02-18 16:32:41 +0100

    reducing header overhead

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,10 @@
-////////////////////////////////////////////////////////////////////////////////
-//                                                                            //
-//                                   PARASUITE                                //
-//                       Paraglider Engineering Framework                     //
-//                       http://parasuite.geisterchor.com                     //
-//                                                                            //
-////////////////////////////////////////////////////////////////////////////////
-//                                                                            //
-// Copyright 2014 Malte Janduda                                               //
-//                                                                            //
-// This file is part of ParaSuite.                                            //
-//                                                                            //
-// ParaSuite is free software: you can redistribute it and/or modify it under //
-// the terms of the GNU General Public License as published by the Free       //
-// Software Foundation, either version 3 of the License, or (at your option)  //
-// any later version.                                                         //
-//                                                                            //
-// ParaSuite is distributed in the hope that it will be useful, but WITHOUT   //
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or      //
-// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for   //
-// more details.                                                              //
-//                                                                            //
-// You should have received a copy of the GNU General Public License along    //
-// with ParaSuite. If not, see http://www.gnu.org/licenses/.                  //
-//                                                                            //
-////////////////////////////////////////////////////////////////////////////////
-
+/*
+ * PARASUITE
+ * http://parasuite.geisterchor.com  
+ *
+ * Copyright (c) 2014 Malte Janduda <mail@malte-janduda.de>
+ * Licensed under the terms of the GNU General Public License, version 3.
+ */
 
 #include <iostream>
 
